3ae3aecfa8cd07d4cda3a2d1465c529bf86214af
KebronDaniel <kebronjrdaniel@gmail.com>
2023-05-22 15:53:03 +0300

    initial commit to mark the start of the project.

---
+++ src/main/java/com/HMS/CareHive/CareHiveApplication.java
@@ -0,0 +1,13 @@
+package com.HMS.CareHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HMS/CareHive/CareHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HMS.CareHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
